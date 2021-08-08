5c9005b83a3385ac770b999831e282ef3c3165c7
muakherul islam <muakherul@gmail.com>
2021-08-09 04:13:57 +0600

    GA added

--- main.py
+++ main.py
@@ -162,5 +162,5 @@ st.markdown(filedownload(df), unsafe_allow_html=True)
 ##GA
 HtmlFile = open('tracking.html', 'r', encoding='utf-8')
 source_code = HtmlFile.read()
-#print(source_code)
+print(source_code)
 components.html(source_code, height=10)
